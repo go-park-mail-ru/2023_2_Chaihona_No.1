2ab57f86f525f4a86563210432f6914b3c631956
Alexander Tarba <sachatarba@rambler.ru>
2023-09-29 14:50:42 +0300

    CH-1: Исправить интерфейс репозитория сессий

--- authorization/session_repository.go
+++ authorization/session_repository.go
@@ -3,6 +3,6 @@ package authorization
 type SessionRepository interface {
 	RegisterNewSession(session Session) error
 	DeleteSession(sessionId string) error
-	CheckSession(sessionId string) (Session, bool)
+	CheckSession(sessionId string) (*Session, bool)
 	GetSessions() ([]Session, error)
 }
