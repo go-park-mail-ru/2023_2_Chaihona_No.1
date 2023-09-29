aee425664c03d4fd7785561f746bc43bda9ac4e3
M0rdovorot <sera.ill@yandex.ru>
2023-09-29 17:06:51 +0300

    CH-6: Добавить роутинг для получения всех постов профиля

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	auth "project/authorization"
+	post "project/post"
 	profile "project/profile"
 	reg "project/registration"
 
@@ -13,12 +14,14 @@ func main() {
 	authHandler := auth.CreateRepoHandler()
 	registrationHandler := reg.CreateRepoHandler()
 	profileHandler := profile.CreateRepoHandler()
+	postHandler := post.CreateRepoHandler()
 
 	r := mux.NewRouter()
 	r.HandleFunc("/api/v1/login", authHandler.Login).Methods("POST")
 	r.HandleFunc("/api/v1/logout", authHandler.Logout).Methods("POST")
 	r.HandleFunc("/api/v1/registration", registrationHandler.SignUp).Methods("POST")
 	r.HandleFunc("/api/v1/profile/{id:[0-9]+}", profileHandler.GetInfo).Methods("GET")
+	r.HandleFunc("/api/v1/profile/{id:[0-9]+}/post", postHandler.GetAllUserPosts).Methods("GET")
 
 	http.ListenAndServe(":8080", r)
 }
