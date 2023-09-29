eb79e7717217520799beca4f448f4427de56a65a
Alexander Tarba <sachatarba@rambler.ru>
2023-09-29 14:52:54 +0300

    CH-2: Исправить интерфейс репозитория пользователей

--- registration/user_repository.go
+++ registration/user_repository.go
@@ -7,6 +7,6 @@ import (
 type UserRepository interface {
 	RegisterNewUser(user model.User) error
 	DeleteUser(userId uint) error
-	CheckUser(userId uint) (model.User, bool)
+	CheckUser(userId uint) (*model.User, bool)
 	GetUsers() ([]model.User, error)
 }
