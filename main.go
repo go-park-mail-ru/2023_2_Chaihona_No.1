71ad39b96d6a1ca7aa6db2315d0b809e4d2c07f5
M0rdovorot <sera.ill@yandex.ru>
2023-10-01 17:12:00 +0300

    CH-6: Перейти на https, добавить обработчик OPTIONS запросов

--- main.go
+++ main.go
@@ -19,11 +19,12 @@ func main() {
 	postHandler := handlers.CreatePostHandlerViaRepos(&rep.Sessions, &postStorage, &rep.Profiles)
 
 	r := mux.NewRouter()
+	r.Methods("OPTIONS").HandlerFunc(handlers.OptionsHandler)
 	r.HandleFunc("/api/v1/login", rep.Login).Methods("POST")
 	// r.HandleFunc("/api/v1/logout", rep.Logout).Methods("POST")
 	r.HandleFunc("/api/v1/registration", rep.Signup).Methods("POST")
 	r.HandleFunc("/api/v1/profile/{id:[0-9]+}", profileHandler.GetInfo).Methods("GET")
 	r.HandleFunc("/api/v1/profile/{id:[0-9]+}/post", postHandler.GetAllUserPosts).Methods("GET")
 
-	http.ListenAndServe(":8081", r)
+	http.ListenAndServeTLS(":8001", "cert.pem", "key.pem", r)
 }
