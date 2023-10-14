73ead77e925e087af03a2608a7232075876c8e65
M0rdovorot <sera.ill@yandex.ru>
2023-10-14 22:26:13 +0300

    CH-3: Добавить тесты, убрать комментарии, поменять название проекта

--- handlers/profile.go
+++ handlers/profile.go
@@ -3,26 +3,23 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
-	auth "project/authorization"
-	reg "project/registration"
 	"strconv"
 
 	"github.com/gorilla/mux"
+
+	auth "github.com/go-park-mail-ru/2023_2_Chaihona_No.1/authorization"
+	reg "github.com/go-park-mail-ru/2023_2_Chaihona_No.1/registration"
 )
 
 type ProfileHandler struct {
-	Session  *auth.SessionRepository
-	Profiles *reg.ProfileRepository
+	Session  auth.SessionRepository
+	Profiles reg.ProfileRepository
 }
 
-// func CreateProfileHandler() *ProfileHandler {
-// 	return &ProfileHandler{
-// 		auth.CreateSessionStorage(),
-// 		reg.CreateProfileStorage(),
-// 	}
-// }
-
-func CreateProfileHandlerViaRepos(session *auth.SessionRepository, profiles *reg.ProfileRepository) *ProfileHandler {
+func CreateProfileHandlerViaRepos(
+	session auth.SessionRepository,
+	profiles reg.ProfileRepository,
+) *ProfileHandler {
 	return &ProfileHandler{
 		session,
 		profiles,
@@ -31,8 +28,8 @@ func CreateProfileHandlerViaRepos(session *auth.SessionRepository, profiles *reg
 
 func (p *ProfileHandler) GetInfo(w http.ResponseWriter, r *http.Request) {
 	AddAllowHeaders(w)
-	if !auth.CheckAuthorization(r, *p.Session) {
-		http.Error(w, `{"error":"unauthorized"}`, 401)
+	if !auth.CheckAuthorization(r, p.Session) {
+		http.Error(w, `{"error":"unauthorized"}`, http.StatusUnauthorized)
 		return
 	}
 
@@ -43,7 +40,7 @@ func (p *ProfileHandler) GetInfo(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	profile, ok := (*p.Profiles).GetProfile(uint(id))
+	profile, ok := p.Profiles.GetProfile(uint(id))
 	if !ok {
 		http.Error(w, `{"error":"db"}`, 500)
 		return
@@ -53,5 +50,10 @@ func (p *ProfileHandler) GetInfo(w http.ResponseWriter, r *http.Request) {
 		"profiles": profile,
 	}
 	w.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(&Result{Body: body})
+	err = json.NewEncoder(w).Encode(&Result{Body: body})
+	if err == nil {
+		w.WriteHeader(http.StatusOK)
+	} else {
+		http.Error(w, `{"error":"json_encoding"}`, http.StatusInternalServerError)
+	}
 }
