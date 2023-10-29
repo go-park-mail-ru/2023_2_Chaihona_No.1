ee84b86a74ab82db6d6c2fc9126514e2717c6031
M0rdovorot <sera.ill@yandex.ru>
2023-10-29 23:56:25 +0300

    CH-26: Добваить ручки для крудов поста

--- internal/handlers/posts_handler.go
+++ internal/handlers/posts_handler.go
@@ -85,3 +85,128 @@ func (p *PostHandler) GetAllUserPosts(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 	}
 }
+
+func (p *PostHandler) ChangePost(w http.ResponseWriter, r *http.Request) {
+	AddAllowHeaders(w)
+	if !auth.CheckAuthorization(r, p.Sessions) {
+		http.Error(w, `{"error":"unauthorized"}`, http.StatusUnauthorized)
+		return
+	}
+
+	vars := mux.Vars(r)
+	_, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, `{"error":"bad id"}`, http.StatusBadRequest)
+		return
+	}
+
+	body := http.MaxBytesReader(w, r.Body, maxBytesToRead)
+
+	decoder := json.NewDecoder(body)
+	post := &model.Post{}
+
+	err = decoder.Decode(post)
+	if err != nil {
+		http.Error(w, `{"error":"wrong_json"}`, http.StatusBadRequest)
+		return
+	}
+
+	errPost := p.Posts.ChangePost(*post)
+
+	// сделал по примеру из 6-ой лекции, возможно, стоит добавить обработку по дефолту в свиче
+	if errPost != nil {
+		switch err.(type) {
+		case postsrep.ErrorPost:
+			errPost := errPost.(postsrep.ErrorPost)
+			if errors.Is(ErrorNotAuthor, errPost.Err) {
+				res := Result{Err: errPost.Err.Error()}
+				errJSON, err := json.Marshal(res)
+				if err != nil {
+					errJSON = []byte{}
+				}
+				http.Error(w, string(errJSON), errPost.StatusCode)
+				return
+			}
+			http.Error(w, `{"error":"db"}`, errPost.StatusCode)
+			return
+		}
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (p *PostHandler) CreateNewPost(w http.ResponseWriter, r *http.Request) {
+	AddAllowHeaders(w)
+	if !auth.CheckAuthorization(r, p.Sessions) {
+		http.Error(w, `{"error":"unauthorized"}`, http.StatusUnauthorized)
+		return
+	}
+
+	body := http.MaxBytesReader(w, r.Body, maxBytesToRead)
+
+	decoder := json.NewDecoder(body)
+	post := &model.Post{}
+
+	err := decoder.Decode(post)
+	if err != nil {
+		http.Error(w, `{"error":"wrong_json"}`, http.StatusBadRequest)
+		return
+	}
+
+	postId, errPost := p.Posts.CreateNewPost(*post)
+
+	// сделал по примеру из 6-ой лекции, возможно, стоит добавить обработку по дефолту в свиче
+	if errPost != nil {
+		switch err.(type) {
+		case postsrep.ErrorPost:
+			errPost := errPost.(postsrep.ErrorPost)
+			if errors.Is(ErrorNotAuthor, errPost.Err) {
+				res := Result{Err: errPost.Err.Error()}
+				errJSON, err := json.Marshal(res)
+				if err != nil {
+					errJSON = []byte{}
+				}
+				http.Error(w, string(errJSON), errPost.StatusCode)
+				return
+			}
+			http.Error(w, `{"error":"db"}`, errPost.StatusCode)
+			return
+		}
+		return
+	}
+
+	bodyResponse := map[string]interface{}{
+		"id": postId,
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(&Result{Body: bodyResponse})
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func (p *PostHandler) DeleteUser(w http.ResponseWriter, r *http.Request) {
+	AddAllowHeaders(w)
+	if !auth.CheckAuthorization(r, p.Sessions) {
+		http.Error(w, `{"error":"unauthorized"}`, http.StatusUnauthorized)
+		return
+	}
+
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, `{"error":"bad id"}`, 400)
+		return
+	}
+
+	err = p.Posts.DeletePost(uint(id))
+	if err != nil {
+		http.Error(w, `{"error":"db"}`, 500)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
