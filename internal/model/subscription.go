7b47f9677d981214711e903fcd2ede38d15a6cc0
M0rdovorot <sera.ill@yandex.ru>
2023-11-27 22:50:24 +0300

    Поправить поиск по авторам

--- internal/model/subscription.go
+++ internal/model/subscription.go
@@ -4,6 +4,6 @@ type Subscription struct {
 	Id                    uint   `json:"id" db:"id"`
 	Subscriber_id         uint   `json:"-" db:"subscriber_id"`
 	Creator_id            uint   `json:"-" db:"creator_id"`
-	Subscription_level_id uint   `json:"-" db:"subscribtion_level_id"`
+	Subscription_level_id uint   `json:"-" db:"subscription_level_id"`
 	CreationDate          string `json:"-" db:"created_at"`
 }
