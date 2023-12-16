56cde6f0ef136cda3b954d98bae207e8f12915ed
M0rdovorot <sera.ill@yandex.ru>
2023-12-17 02:30:35 +0300

    Подключить окончательно API оплаты

--- internal/handlers/payment_handler.go
+++ internal/handlers/payment_handler.go
@@ -72,7 +72,9 @@ func (p *PaymentHandler) DonateStratagy(ctx context.Context, form PaymentForm) (
 	}
 
 	payment.UUID = responseUkassa.Id
+	fmt.Println(responseUkassa.Amount.Value)
 	splitedValue := strings.Split(responseUkassa.Amount.Value, ".")
+	fmt.Println(splitedValue)
 	integer, err := strconv.Atoi(splitedValue[0])
 	if err != nil {
 		//think
